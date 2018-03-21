0c33c5ca66657e8fef8b69c30c278a0b962ffad2
unknown <mitchelljohnston@hotmail.ca>
2018-03-20 18:41:33 -0700

    orientation

--- Main/Main.ino
+++ Main/Main.ino
@@ -4,12 +4,42 @@
 #include <TurnSensor.h>
 
 
-void setup() {
-  // put your setup code here, to run once:
+//int goal[2] = {3,4};
 
+int cell_counter = 0;
+//int next_cell[2] = 0;
+
+double* x_hat = 0;
+double* y_hat = 0;
+double* theta_hat =0;
+
+int intial[2] = {3,4}; //initial cell location
+
+void setup() {
+
+  turnSensorSetup(); // intialize Gyro
+  delay(500);
+  turnSensorReset(); // Reset Orientation to zero
+
+  // after being placed fetch best estimate of location (uses kevins IR function)
+  //runs once
+
+  // determine array of cells to visit on our way to finish
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+
+  //update next_cell
+  
+
+  //initiate error from next_cell's center
+  
+
+  // fetch applicable sensor data and weight as necessary for measurement
+  
+  //
+
+  cell_counter += 1;
 }
